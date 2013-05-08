ef04c58dc312c0165b349de61381ff5ebb9eed44
Tristan Hearn <tristanhearn@gmail.com>
2013-05-08 19:01:58 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+opencv-data-plotter
+=======================
+A python code for fast, real-time 2d data plotting for codes that already have opencv as a dependency. 
+    
---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(name='opencv-data-plotter',
+    version='0.1',
+    install_requires=[],
+    description="A python code for fast, real-time 2d data plotting for codes that already have opencv as a dependency. ",
+    author='Tristan Hearn',
+    author_email='tristanhearn@gmail.com',
+    url='https://github.com/thearn/opencv-data-plotter',
+    license='Apache 2.0',
+    packages=['src'],
+)
